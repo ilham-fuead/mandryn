e8009bc227f3ac78972a9989ec33ce698b2136b4
Mohd Ilhammuddin Bin Mohd Fuead <ilham.fuead@gmail.com>
2017-11-20 10:23:14 +0000

    inputFilter

--- src/mandryn_v1/InputsFilter.php
+++ src/mandryn_v1/InputsFilter.php
@@ -27,53 +27,13 @@ class InputsFilter {
     public function getFilteredInputs() {
         return filter_var_array($this->inputs->toArray(), $this->inputsFilterDefinition->toArray());
     }
+    
+    public function addFilterOption($fieldName,array $option){
+        if(array_key_exists('options', $this->inputsFilterDefinition->{$fieldName})){
+            array_push($this->inputsFilterDefinition->{$fieldName}["options"],$option);
+        } else {
+            array_push($this->inputsFilterDefinition->{$fieldName},["options"=>$option]);
+        }
+    }
 
 }
-
-$mgObj = new MagicObject();
-
-$inputs = [
-    ["name" => "txtAge", "value" => "9", "type" => "int"],
-    ["name" => "txtMail", "value" => "kl@kl.>>>com", "type" => "str"],
-    ["name" => "txtWages", "value" => "10000.98", "type" => "flo"]
-];
-
-$fields = [
-    "txtAge" => "9",
-    "txtMail" => "kl@kl.>>>com",
-    "txtWages" => "10000.98"
-];
-
-$filterArray = [
-    "int" => FILTER_VALIDATE_INT,
-    "flo" => FILTER_VALIDATE_FLOAT,
-    "str" => FILTER_SANITIZE_STRING
-];
-
-/**
-  foreach ($inputs as $input) {
-  $mgObj->{$input["name"]} = ["filter" => $filterArray[$input["type"]]];
-  }
-
-  echo '<pre>';
-
-  print_r($fields);
-
-  echo '<p>';
-
-  print_r($mgObj->toArray());
-
-  echo '<p>';
-
-  var_dump(filter_var_array($fields, $mgObj->toArray()));
- * 
- */
-$def = new InputFilter($inputs);
-
-echo '<pre>';
-
-print_r($def->getDefinition());
-
-echo '<p>';
-
-print_r($def->getFilteredInputs());
