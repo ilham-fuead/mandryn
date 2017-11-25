a2fee6144fac8e663ceb3b656f1725ffe8355cda
unknown <User@NB17_BPMS7.jpa.gov.my>
2017-11-25 03:15:47 +0000

    cd

--- src/mandryn_v1/InputsFilter.php
+++ src/mandryn_v1/InputsFilter.php
@@ -1,8 +1,10 @@
 <?php
+
 class InputsFilter {
 
     private $inputsFilterDefinition;
     private $inputs;
+    private $autoSanitize;
 
     const filterArray = [
         "int" => FILTER_VALIDATE_INT,
@@ -10,29 +12,59 @@ class InputsFilter {
         "str" => FILTER_SANITIZE_STRING
     ];
 
-    public function __construct(array $inputs) {
-        $this->inputsFilterDefinition = new MagicObject();
-        $this->inputs=new MagicObject();
+    public function __construct($autoSanitize = false) {
+        $this->inputs = [];
+        $this->inputsFilterDefinition = [];
+        $this->autoSanitize = $autoSanitize;
+    }
 
-        foreach ($inputs as $input) {
-            $this->inputs->{$input["name"]}=$input["value"];
-            $this->inputsFilterDefinition->{$input["name"]} = ["filter" => self::filterArray[$input["type"]]];
+    public function setSingleInput(array $input, $FILTER_VALIDATE_OR_SANITIZE = "") {
+        $this->inputs[$input["name"]] = $input["value"];
+        if ($FILTER_VALIDATE_OR_SANITIZE === "") {
+            $this->setFilterDefinition($input["name"], self::filterArray[$input["type"]]);
+        } else {
+            $this->setFilterDefinition($input["name"], $FILTER_VALIDATE_OR_SANITIZE);
         }
     }
 
+    public function setMultipleInputs(array $inputs) {
+        foreach ($inputs as $input) {
+            $this->setSingleInput($input);
+        }
+    }
+
+    public function setFilterDefinition($inputName, $FILTER_VALIDATE_OR_SANITIZE) {
+        $this->inputsFilterDefinition[$inputName] = [];
+        $this->inputsFilterDefinition[$inputName]["filter"] = $FILTER_VALIDATE_OR_SANITIZE;
+    }
+
+    public function deleteFilterDefinition($inputName) {
+        unset($this->inputsFilterDefinition[$inputName]);
+    }
+
     public function getDefinition() {
-        return $this->inputsFilterDefinition->toArray();
+        return $this->inputsFilterDefinition;
     }
 
     public function getFilteredInputs() {
-        return filter_var_array($this->inputs->toArray(), $this->inputsFilterDefinition->toArray());
+        return filter_var_array($this->inputs, $this->inputsFilterDefinition);
     }
-    
-    public function addFilterOption($fieldName,array $option){
-        if(array_key_exists('options', $this->inputsFilterDefinition->{$fieldName})){
-            array_push($this->inputsFilterDefinition->{$fieldName}["options"],$option);
+
+    public function addFilterOptions($inputName, $optionName, $optionValue) {
+        if (array_key_exists('options', $this->inputsFilterDefinition[$inputName])) {
+            $this->inputsFilterDefinition[$inputName]["options"][$optionName] = $optionValue;
         } else {
-            array_push($this->inputsFilterDefinition->{$fieldName},["options"=>$option]);
+            $this->inputsFilterDefinition[$inputName]["options"] = [];
+            $this->inputsFilterDefinition[$inputName]["options"][$optionName] = $optionValue;
+        }
+    }
+
+    public function addFilterFlags($inputName, $flagName, $flagValue) {
+        if (array_key_exists('flags', $this->inputsFilterDefinition[$inputName])) {
+            $this->inputsFilterDefinition[$inputName]["flags"][$flagName] = $flagValue;
+        } else {
+            $this->inputsFilterDefinition[$inputName]["flags"] = [];
+            $this->inputsFilterDefinition[$inputName]["flags"][$flagName] = $flagValue;
         }
     }
 
