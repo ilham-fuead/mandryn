8ad96c8a8b651a738d38cef2e11cdca38f4bfea4
Rawuf Abdel <codeart.aes@gmail.com>
2023-03-06 08:07:37 +0000

    DOQuery (PDO version of DDQuery) -- query function ver 1.0

---
+++ src/mandryn_v1/DOQuery.php
@@ -0,0 +1,366 @@
+<?php
+
+class ConnectionDetail
+{
+
+    public $host;
+    public $username;
+    public $password;
+    public $database_name;
+    public $dsn;
+
+    public function __construct($host, $username, $password, $database_name, $dsn)
+    {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->database_name = $database_name;
+        $this->dsn=$dsn;
+    }
+
+}
+
+class DB
+{
+
+    protected $db_driver;
+    private $db_host;
+    private $db_username;
+    private $db_password;
+    private $db_name;
+    private $dsn;
+    protected $db_link;
+    private $connInfo;
+
+    public function __construct($host, $username, $password, $database_name, $db_driver)
+    {
+        $this->db_link=null;
+        $this->setLinkProperties($host, $username, $password, $database_name, $db_driver);
+        $this->openLink();
+        //$this->setMinum();
+    }
+
+    public function setLinkProperties($host, $username, $password, $database_name, $db_driver)
+    {
+        $this->db_host = $host;
+        $this->db_username = $username;
+        $this->db_password = $password;
+        $this->db_name = $database_name;
+        $this->db_driver= $db_driver;
+        $this->dsn="{$this->db_driver}:host={$this->db_host};dbname={$this->db_name}";
+    }
+
+    public function getConnectionDetail()
+    {
+        $this->connInfo = new ConnectionDetail($this->db_host, $this->db_username, $this->db_password, $this->db_name, $this->dsn);
+        return $this->connInfo;
+    }
+
+    private function openLink()
+    {
+        $this->closeLink();
+        try {
+            $dbh=new PDO($this->dsn, $this->db_username, $this->db_password);
+            $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, TRUE);
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->db_link=$dbh;
+        } catch (PDOException $e) {
+            header("{$_SERVER['SERVER_PROTOCOL']} 500 {$e->getMessage()}");
+            exit;
+            //die($e->getMessage());
+        }
+    }
+
+    public function getLink()
+    {
+        return $this->db_link;
+    }
+
+    private function closeLink()
+    {
+        if ($this->db_link){
+            $this->db_link=null;
+        }
+            
+    }
+
+    public function __destruct()
+    {
+        $this->closeLink();
+    }
+    
+    public function escape_string($unEscapeStr){
+        return mysqli_real_escape_string($this->db_link,$unEscapeStr);
+    }    
+
+    private function setMinum()
+    {
+        echo '<h4>Error occured while trying to connect mysql resource pooling.</h4><br>
+          Please review below mentioned recommendation for rectification action:<p>
+          
+          1. Go to setting, click Sana\'a<br>
+          2. In the menu, browse last page<br>
+          3. Please order ADANI MILK POT, system operation will resume as normal!
+          ';
+        exit(0);
+    }
+
+}
+
+class DOQuery extends DB
+{
+    const MODE = 'DEV'; // DEV||PROD
+    const MAX_TOTAL_INSTANCE=3;
+    
+    private static $totalInstance=0;
+
+    private $db_sql;
+    private $db_sql_params;
+    private $db_result;
+    private $commandType;
+    private $transactionEnable;
+    private $executionStatusArray;
+    private $num_rows;
+    
+    public function __construct($host, $username, $password, $database_name, $is_DB_connection_limit_enforced=true, $db_driver='mysql')
+    {        
+        try{
+            if(DOQuery::$totalInstance>DOQuery::MAX_TOTAL_INSTANCE && $is_DB_connection_limit_enforced){
+                $errMsg = 'DBQuery error : Maximum connection limit exceeded!';
+                throw new Exception($errMsg);
+            }
+        } catch (Exception $e) {
+            header("{$_SERVER['SERVER_PROTOCOL']} 500 {$e->getMessage()}");
+            exit;
+        }
+        
+        parent::__construct($host, $username, $password, $database_name, $db_driver);
+        $this->db_sql_params = array();
+        DOQuery::$totalInstance+=1;               
+        $this->executionStatusArray=[];
+        $this->num_rows=0;
+        
+    }
+    
+    public function enableTransaction(){
+        $this->transactionEnable=TRUE;
+        mysqli_autocommit($this->db_link, FALSE);
+    }
+    
+    public function disableTransaction(){
+        $this->transactionEnable=FALSE;
+        mysqli_autocommit($this->db_link, TRUE);
+    }
+    
+    public function commitTransaction(){
+        $clearedStatusFlag=TRUE;
+        foreach ($this->executionStatusArray as $execution){
+            if($execution->status===FALSE){
+                $clearedStatusFlag=FALSE;
+                break;
+            }
+        }       
+        
+        $copiedExecutionStatusArray=$this->executionStatusArray;
+        $this->executionStatusArray=[];
+        
+        if($clearedStatusFlag===TRUE){
+           return mysqli_commit($this->db_link); 
+        }else{
+            mysqli_rollback($this->db_link);
+            return $copiedExecutionStatusArray;
+        }       
+    }
+        
+    public static function getTotalInstanceCount(){
+        return DBQuery::$totalInstance;
+    }
+
+    public function setSQL_Statement($sql)
+    {
+        //$this->freeRecordset();
+        $this->db_sql = $sql;
+    }
+
+    private function performQuery()
+    {
+        $dbh=$this->db_link;
+       
+        $this->db_result =$dbh->query($this->db_sql)->fetchAll();
+    }
+
+    public function runSQL_Query()
+    {
+        $this->performQuery();
+        try {
+            if (is_bool($this->db_result)) {
+                $this->commandType = "non query";
+                if (DBQuery::MODE == 'DEV' && $this->db_result === FALSE) {
+                    $errMsg = 'Database error: ' . mysqli_error($this->db_link);
+                    throw new Exception($errMsg);
+                } else if ($this->db_result === FALSE) {
+                    $errMsg = 'Database error';
+                    throw new Exception($errMsg);                    
+                }
+            } else {
+                $this->commandType = "query";
+                $this->countNumRows();
+            }
+        }catch (Exception $e) {
+            header("{$_SERVER['SERVER_PROTOCOL']} 500 {$e->getMessage()}");
+            exit;
+        }
+    }
+    
+    private function countNumRows(){
+        if($this->db_driver==='mysql'){
+            $this->num_rows= $this->db_link->query("SELECT COUNT(*) FROM ({$this->db_sql}) AS qtbl")->fetchColumn();
+        }
+    }
+
+    public function executeNon_Query()
+    {
+        $this->performQuery();
+        $this->commandType = "non query";
+        
+        if($this->transactionEnable){
+            $this->recordExecutionStatus();
+        }
+    }
+    
+    private function recordExecutionStatus(){
+        $SQL_execution=new MagicObject();
+        
+        if($this->getCommandStatus()===TRUE){
+            $SQL_execution->status=TRUE;
+            $SQL_execution->message="success";
+        }else{
+            $SQL_execution->status=FALSE;
+            $SQL_execution->message=mysqli_error($this->getLink());;
+        }        
+        
+        $this->executionStatusArray[]=$SQL_execution;
+    }
+
+    public function getQueryResult()
+    {
+        if ($this->commandType == "query") {
+            return $this->db_result;
+        } else {
+            throw new Exception('No recordset returned!');
+        }
+    }
+
+    public function getCommandStatus()
+    {
+        if ($this->commandType == "non query") {
+            return $this->db_result;
+        } else {
+            throw new Exception('No command status returned, instead a recordset is returned');
+        }
+    }
+
+    public function yieldRow($resulttype = MYSQLI_ASSOC)
+    {
+        while ($row = mysqli_fetch_array($this->db_result, $resulttype)) {
+            yield $row;
+        }
+    }
+
+    public function fetchRow($resulttype = MYSQLI_ASSOC)
+    {
+        return mysqli_fetch_array($this->db_result, $resulttype);
+    }
+    
+    public function getRowsInJSON()
+    {
+        $rows = [];
+        
+        foreach ($this->yieldRow() as $row) {
+            $rows[] = $row;
+        }
+        
+        $rowsJSON = json_encode($rows);
+        unset($rows);
+        return $rowsJSON;
+    }
+    
+    public function getRowsInArray()
+    {
+        $rows = [];
+        
+        foreach ($this->yieldRow() as $row) {
+            $rows[] = $row;
+        }
+        
+        return $rows;
+    }
+
+    public function isHavingRecordRow()
+    {
+        if ($this->commandType == "query") {
+            if (mysqli_num_rows($this->db_result) > 0) {
+                return TRUE;
+            } else {
+                return FALSE;
+            }
+        }
+    }
+    
+    public function getNumRows(){
+        return $this->num_rows;
+    }
+    
+    public function getSqlString()
+    {
+        return $this->db_sql;
+    }
+
+    private function freeRecordset()
+    {
+        if ($this->commandType == "query") {
+            if ($this->db_result && !is_bool($this->db_result)) {
+                //mysqli_free_result($this->db_result);
+            }
+        }
+    }
+
+    public function __destruct()
+    {
+        DOQuery::$totalInstance-=1;
+        $this->freeRecordset();
+        parent::__destruct();
+        unset($this->db_sql_params);
+        unset($this->executionStatusArray);
+    }
+
+}
+
+/*
+ * 
+ * 
+ * Version    : 1.3 [ Released Date: 10 June 2015 ]
+ * Updated By : Mohd Ilhammuddin Bin Mohd Fuead
+ * Remarks    : Added Error handling(Try Catch Exception) statement 
+ *              when executing mysqli_connect and display error accordingly        
+ *
+ * Version    : 1.2 [ Released Date: 08 June 2012 ]
+ * Updated By : Mohd Ilhammuddin Bin Mohd Fuead
+ * Remarks    : i)  Solved bug on freeing a non return resultset query.
+ *              ii) Set generic error regarding mysqli error due to incorrect SQL
+ *
+ * Version    : 1.1 [ Released Date: 27 Oct 2011 ]
+ * Updated By : Mohd Ilhammuddin Bin Mohd Fuead
+ * Remarks    : Added new helper method (freeRecordset) for better memory management
+ *              where returned and used recordset will immediately free from memory 
+ *              when new SQL statement set to DBQuery object sql property.
+ *
+ * Version    : 1.0 [ Released Date: 5 May 2011 ]
+ * Developer  : Mohd Ilhammuddin Bin Mohd Fuead
+ * Description/Remarks:
+ *                     A Utility class for constructing valid SQL statement to
+ *                     be consume by other main classes. This version support all
+ *                     standard SQL Command with limitation on single table only operation.
+ *
+ */
+?>
